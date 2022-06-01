0a1fccc8623c88b0186a14389cdefb91be64f3d5
Danielar0w0 <danielar0000@gmail.com>
2022-06-01 19:57:42 +0100

    Started implementing Sign Up Page

--- delivery-service/src/components/SignUp/SignUp.js
+++ delivery-service/src/components/SignUp/SignUp.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import {Button, Col, Container, Form, Image, Row} from "react-bootstrap";
+import Input from "../Input/Input";
+import Switch from "../Switch/Switch";
 
 const SignUp = () => {
 
-    return (
-        <div data-testid="Profile">
-            Profile Component
-        </div>
+    const [currentUser, setCurrentUser] = React.useState("Delivery Person");
+    const handleSwitch = () => {
+        if (currentUser === "Delivery Person") {
+            setCurrentUser("Verified Store");
+        } else {
+            setCurrentUser("Delivery Person");
+        }
+    };
+
+
+    return(
+        <Container className="justify-content-center text-center d-flex my-4">
+            <Col className="col-5 mb-4">
+                <Row className="justify-content-center d-flex">
+                    <Image className="w-50" src='/Delivery Service.png' alt='Delivery Service logo' />
+                </Row>
+                <Row className='my-4'>
+                    <h2 style={{fontWeight: "bold"}}>Sign Up</h2>
+                    <span>You already have an account? <a className='link' href='#'>Login</a>!</span>
+                </Row>
+                <Row className='justify-content-center d-flex py-4'>
+                    <Switch selected={currentUser} on_value_changed={handleSwitch.bind(this)}/>
+                </Row>
+                <Row>
+                    <form className='input-form'>
+                        <Input type="text" label="Name" value="name"/>
+                        <Input type="email" label="Email" value="email"/>
+                        <Input type="phone" label="Phone No." value="phone"/>
+                        <Input type="password" label="Password" value="password"/>
+                        <Input type="password" label="Re-enter Password" value="confirmPassword"/>
+                    </form>
+                </Row>
+                <Row className='justify-content-center text-center d-flex mt-4'>
+                    <p className="text-muted">Signing up as {currentUser}</p>
+                    <Button className="w-25" type="submit" class="sign-up" style={{borderRadius: "15px"}}>Sign Up</Button>
+                </Row>
+            </Col>
+        </Container>
     );
 };
 
---
+++ delivery-service/src/components/Switch/Switch.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Col, Form} from "react-bootstrap";
+import './Switch.css';
+
+
+const Switch = (props) =>  {
+
+    let deliveryColor;
+    let storeColor;
+    if (props.selected === 'Delivery Person') {
+        deliveryColor = "#2F80ED";
+        storeColor = '#3a3a3a';
+    } else {
+        deliveryColor = '#3a3a3a';
+        storeColor = "#2F80ED";
+    }
+
+    return(
+        <>
+            <Col  className="col-2 justify-content-center d-flex">
+                <h5 style={{textTransform: "uppercase", color: deliveryColor}}>Delivery Person</h5>
+            </Col>
+            <Col className="col-4 align-items-center justify-content-center d-flex">
+                <Form>
+                    <Form.Check
+                        type="switch"
+                        id="singup-switch"
+                        onChange={() => props.on_value_changed()}
+                    />
+                </Form>
+            </Col>
+            <Col  className="col-2 justify-content-center d-flex">
+                <h5 style={{textTransform: "uppercase", color: storeColor}}>Verified Store</h5>
+            </Col>
+        </>
+    );
+
+};
+
+export default Switch;

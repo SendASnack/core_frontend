436ab5930d8c6c8f27f90ef59228d7ed00a4875e
Danielar0w0 <danielar0000@gmail.com>
2022-06-18 21:57:42 +0100

    Revert "Finished Profile pages"
    This reverts commit e791922f189e0f95c6916304a16a86e341ffe887.

--- delivery-service/src/components/ProfileRider/ProfileRider.js
+++ delivery-service/src/components/ProfileRider/ProfileRider.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Navbar from "../Navbar/Navbar";
 import {Button, Card, Col, Form, Image, Row} from "react-bootstrap";
-import PasswordForm from "../Forms/PasswordForm/PasswordForm";
-import PhoneNoForm from "../Forms/PhoneNoForm/PhoneNoForm";
-import RideForm from "../Forms/RideForm/RideForm";
+import PasswordForm from "../PasswordForm/PasswordForm";
+import PhoneNoForm from "../PhoneNoForm/PhoneNoForm";
+import RideForm from "../Rider/RideForm/RideForm";
 
 const ProfileRider = () => {
 

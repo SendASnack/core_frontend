89016aa3058ecaf96bfa98f6e2a8e7328ac58033
Danielar0w0 <danielar0000@gmail.com>
2022-06-19 21:17:59 +0100

    Implemented Business Home page

--- delivery-service/src/components/Businness/HomeBusiness/HomeBusiness.js
+++ delivery-service/src/components/Businness/HomeBusiness/HomeBusiness.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import {Card, Col, Row} from "react-bootstrap";
+import React, {useEffect} from 'react';
+import {Card, Col, Form, Image, Row} from "react-bootstrap";
 import Navbar from "../../Navbar/Navbar";
 import OrderPanelsList from "../../Businness/OrderPanelsList/OrderPanelsList";
 import {Line} from 'react-chartjs-2';
 
-import { Chart as ChartJS, registerables } from 'chart.js';
+import {Chart as ChartJS, registerables} from 'chart.js';
+import UserCard from "../UserCard/UserCard";
 
 if (registerables)
     ChartJS.register(...registerables);
 
 const HomeBusiness = () => {
 
+    const [nOngoingOrders, setNOngoingOrders] = React.useState(0);
     const allOrders = [
         {
             "id": 1,
@@ -70,19 +72,36 @@ const HomeBusiness = () => {
         }
     ];
 
+    useEffect(() => {
+        if (allOrders) {
+            let ongoing = 0;
+            for (let o of allOrders) {
+                if (new Date(o.deliveryTime) > new Date()) {
+                    ongoing++;
+                }
+            }
+            setNOngoingOrders(ongoing);
+        }
+    }, [allOrders]);
+
+    let days = [];
+    for (let i = 0; i < 7; i++) {
+        days.push(new Date((new Date()).valueOf() - 1000*60*60*24*(6-i)).toLocaleDateString());
+    }
+
     const data = {
         // x-axis label values
-        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"],
-            datasets: [
+        labels: [days[0], days[1], days[2], days[3], days[4], days[5], days[6]],
+        datasets: [
             {
                 label: "# of orders",
                 // y-axis data plotting values
-                data: [200, 300, 1300, 520, 2000, 350,150],
+                data: [200, 300, 1300, 520, 2000, 350, 150],
                 fill: false,
-                borderWidth:4,
+                borderWidth: 4,
                 backgroundColor: "#2F80ED",
-                borderColor:'#2F80ED',
-                responsive:true
+                borderColor: '#2F80ED',
+                responsive: true
             },
         ],
     }
@@ -90,17 +109,25 @@ const HomeBusiness = () => {
     return (
         <Row className="justify-content-center text-center d-flex p-0" data-testid="HomeBusiness">
             <Navbar active="Home"/>
-            {/*
             <Row className="mt-5 justify-content-center d-flex">
                 <Col className="col-4 p-0 mx-4">
                     <UserCard/>
                 </Col>
                 <Col className="col-3 p-0 mx-4">
+                    <Card className="shadow bg-white p-4 h-100" style={{borderRadius: "20px", border: "none"}}
+                          data-testid="status-card">
+                        <Row className="align-items-center text-start d-flex h-100 mx-2 p-3">
+                            <h2 className="sub-title">Total Orders:</h2>
+                            <h2 className="details" style={{color: "#2f80ed"}}>{allOrders.length} order(s)</h2>
+                            <h2 className="sub-title">Ongoing Orders:</h2>
+                            <h2 className="details" style={{color: "#2f80ed"}}>{nOngoingOrders} order(s)</h2>
+
+                        </Row>
+                    </Card>
                 </Col>
             </Row>
-            */}
             <Row className="mt-5 justify-content-center d-flex">
-                <Col className="col-7 p-0">
+                <Col className="col-8 p-0">
                     <Card className="shadow p-5 bg-white my-4" style={{borderRadius: "20px", border: "none"}}>
                         <h2 className="title mb-4">Dashboard</h2>
                         <Line data={data}/>
@@ -115,8 +142,5 @@ const HomeBusiness = () => {
         </Row>
     );
 }
-HomeBusiness.propTypes = {};
-
-HomeBusiness.defaultProps = {};
 
 export default HomeBusiness;
